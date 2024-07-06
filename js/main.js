0861163ef77f6efea6cbe2111cffceaf71919a8a
George Zaklepniy <zak.taio2@gmail.com>
2024-07-06 09:42:44 +0300

    Сохранение изменений перед слиянием

--- js/main.js
+++ js/main.js
@@ -54,3 +54,11 @@ $('.burger, .overlay').on('click', function (e) {
 	$('.overlay').toggleClass('overlay--show')
 	$('.burger').toggleClass('close-btn')
 })
+	$(
+		'.header-nav-link'
+	).on('click', function (e) {
+		e.preventDefault()
+		var id = $(this).attr('href'),
+			top = $(id).offset().top
+		$('body,html').animate({ scrollTop: top }, 1000)
+	})
